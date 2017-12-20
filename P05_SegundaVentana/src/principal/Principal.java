4e85b6e8fa09c16f04a0185536fe2414d04f5c19
vicentdocedasanchis <enricvds@gmail.com>
2017-12-20 18:36:52 +0100

    Añadidos algunos comentarios

--- P05_SegundaVentana/src/principal/Principal.java
+++ P05_SegundaVentana/src/principal/Principal.java
@@ -4,17 +4,21 @@ package principal;
 import ventanas.VentanaJuego;
 import ventanas.VentanaLogin;
 
-
 /*
  * EN LA CLASE PRINCIPAL SE CREAN Y LANZAN LAS VENTANAS DEL JUEGO
  * */
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * Necesito tener ya creado un objeto vj de la clase VentanaJuego antes de crear
+		 * la ventana de login porque se lo voy a pasar a login, sino daria error
+		 */
 		// Se crea la ventana del juego
 		VentanaJuego vJ = new VentanaJuego();
 
-		// Se crea la ventana de login y se le pasa como referencia la ventana de juego
+		// Se crea la ventana de login y se le pasa la ventana de juego
 		// La ventana de login interactua con la clase jugador y con la ventana de juego
 		VentanaLogin login = new VentanaLogin(vJ);
 		// Se hace visible la ventana de login
